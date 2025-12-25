6a91f276d7f6e13cd4c6a92da0ce50f57211fb2c
Gunnell <59669870+Gunnell@users.noreply.github.com>
2025-12-25 23:59:18 +0300

    Animate Goal cards

--- Assets/Match Lab/Scripts/UI/GoalCard.cs
+++ Assets/Match Lab/Scripts/UI/GoalCard.cs
@@ -7,16 +7,19 @@ public class GoalCard : MonoBehaviour
     [Header(" Elements ")]
     [SerializeField] private TextMeshProUGUI amountText;
     [SerializeField] private Image iconImage;
+    [SerializeField] private GameObject backOfCard;
+    [SerializeField] private GameObject checkMark;
+    [SerializeField] private Animator completeAnimator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        completeAnimator.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        backOfCard.SetActive(Vector3.Dot(transform.forward, Vector3.forward) < 0);
     }
 
     public void Configure(int initialAmount, Sprite goalIcon)
@@ -29,10 +32,24 @@ public class GoalCard : MonoBehaviour
     public void UpdateAmount(int newAmount)
     {
         amountText.text = newAmount.ToString();
+        Bump();
+    }
+
+    private void Bump()
+    {
+        LeanTween.cancel(gameObject);
+        transform.localScale = Vector3.one;
+        LeanTween.scale(gameObject, Vector3.one * 1.1f, .1f).setLoopPingPong(1);
     }
 
     public void Complete()
     {
-        gameObject.SetActive(false);
+        completeAnimator.enabled = true;
+
+        checkMark.SetActive(true);
+        amountText.gameObject.SetActive(false);
+        
+        completeAnimator.Play("Complete");
+        
     }
 }
